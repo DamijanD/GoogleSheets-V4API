ca81f116f465c5e7cb498d053c181df43c09b6c5
DamijanD <damijan.dolenec@gmail.com>
2018-10-27 19:45:15 +0200

    Support multiple files

--- GoogleSheetsTest/Program.cs
+++ GoogleSheetsTest/Program.cs
@@ -30,116 +30,119 @@ namespace GoogleSheetsTest
                 ApplicationName = ApplicationName,
             });
 
-            // Define request parameters.
-            //String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
-            //String range = "Class Data!A2:E";
-
             string spreadsheetId = System.Configuration.ConfigurationManager.AppSettings["spreadsheetId"];
-            string sheetNo = System.Configuration.ConfigurationManager.AppSettings["sheetNo"];
-            string sheetName = "MB" + sheetNo;
-            string range = sheetName + "!A:AR";
 
-            var spreadsheet = service.Spreadsheets.Get(spreadsheetId).Execute();
-            var sheet = spreadsheet.Sheets.FirstOrDefault(x => x.Properties.Title == sheetName);
+            string inputPath = System.Configuration.ConfigurationManager.AppSettings["InputPath"];
+            string filePrefix = System.Configuration.ConfigurationManager.AppSettings["FilePrefix"];
 
-            if (sheet == null)
+            var files = System.IO.Directory.EnumerateFiles(inputPath, filePrefix + "*.txt");
+
+            Console.WriteLine("Found {0} in {1}", files.Count(), inputPath + filePrefix);
+
+            foreach (var file in files)
             {
-                sheet = CreateSheet(service, spreadsheetId, sheetName, ref spreadsheet);
-            }
+                Console.WriteLine("Processing {0}", file);
 
-            if (sheet == null)
-            {
-                Console.WriteLine("Unable to get sheet {0}", sheetName);
-                return;
-            }
+                System.IO.FileInfo fi = new FileInfo(file);
 
-            Console.WriteLine("Using sheet {0}", sheetName);
+                string sheetName = fi.Name.Replace(filePrefix, "").Replace(".txt", "");
+                string range = sheetName + "!A:AR";
 
-            string inputFilename = System.Configuration.ConfigurationManager.AppSettings["InputFile"];
+                var spreadsheet = service.Spreadsheets.Get(spreadsheetId).Execute();
+                var sheet = spreadsheet.Sheets.FirstOrDefault(x => x.Properties.Title == sheetName);
 
-            Console.WriteLine("Using input file {0}", inputFilename);
-
-            string inputData;
-
-            using (var stream = new FileStream(inputFilename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            using (var reader = new StreamReader(stream))
-            {
-                inputData = reader.ReadToEnd();
-                reader.Close();
-            }
-
-            if (string.IsNullOrEmpty(inputData))
-            {
-                Console.WriteLine("File empty!");
-                return;
-            }
-
-            var inputLines = inputData.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-
-            Console.WriteLine("Loaded lines {0}", inputLines.Length);
-
-            var rangeValues = service.Spreadsheets.Values.Get(spreadsheetId, range).Execute();
-            var rangeLines = 0;
-
-            if (rangeValues.Values != null)
-                rangeLines = rangeValues.Values.Count;
-
-            Console.WriteLine("Range lines {0}", rangeLines);
-
-            if (rangeLines > 0)
-            {
-                //we could seach by value, but we know that line numbers are the same...
-                var lastLine = rangeValues.Values[rangeLines - 1];
-            }
-
-            var newData = new List<IList<object>>();
-
-            for (int i = rangeLines; i < inputLines.Length; i++)
-            {
-                var splitedInputLine = inputLines[i].Split(";".ToCharArray());
-
-                  var date = DateTime.ParseExact(splitedInputLine[0], "dd.MM.yy", CultureInfo.InvariantCulture);
-                  var time = DateTime.ParseExact(splitedInputLine[1], "HH:mm", CultureInfo.InvariantCulture);
-                  var timeOnly = time - time.Date;
-                  var values = splitedInputLine.Skip(2).Select(x => decimal.Parse(x)).Cast<object>().ToList();
-
-                /*  var data = new List<object>();
-                  data.Add(new DateTime(date.Year, date.Month, date.Day, timeOnly.Hours, timeOnly.Minutes, 0));
-                  data.Add(timeOnly);
-                  data.AddRange(values);*/
-
-                //  var data = new List<object>(splitedInputLine);
-
-                var data = new List<object>();
-                data.Add(splitedInputLine[0]);
-                data.Add(splitedInputLine[1]);
-                data.AddRange(values);
-
-                newData.Add(data);
-                
-                if (i % 100 == 0)
+                if (sheet == null)
                 {
-                    Console.Write(".");
+                    sheet = CreateSheet(service, spreadsheetId, sheetName, ref spreadsheet);
+                }
+
+                if (sheet == null)
+                {
+                    Console.WriteLine("Unable to get sheet {0}", sheetName);
+                    continue;
+                }
+
+                Console.WriteLine("Using sheet {0}", sheetName);
+
+                string inputData;
+
+                using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var reader = new StreamReader(stream))
+                {
+                    inputData = reader.ReadToEnd();
+                    reader.Close();
+                }
+
+                if (string.IsNullOrEmpty(inputData))
+                {
+                    Console.WriteLine("File empty!");
+                    continue;
+                }
+
+                var inputLines = inputData.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Console.WriteLine("Loaded lines {0}", inputLines.Length);
+
+                var rangeValues = service.Spreadsheets.Values.Get(spreadsheetId, range).Execute();
+                var rangeLines = 0;
+
+                if (rangeValues.Values != null)
+                    rangeLines = rangeValues.Values.Count;
+
+                Console.WriteLine("Range lines {0}", rangeLines);
+
+                if (rangeLines > 0)
+                {
+                    //we could seach by value, but we know that line numbers are the same...
+                    //var lastLine = rangeValues.Values[rangeLines - 1];
+                }
+
+                var newData = new List<IList<object>>();
+
+                for (int i = rangeLines; i < inputLines.Length; i++)
+                {
+                    var splitedInputLine = inputLines[i].Split(";".ToCharArray());
+
+                    var date = DateTime.ParseExact(splitedInputLine[0], "dd.MM.yy", CultureInfo.InvariantCulture);
+                    var time = DateTime.ParseExact(splitedInputLine[1], "HH:mm", CultureInfo.InvariantCulture);
+                    var timeOnly = time - time.Date;
+                    var values = splitedInputLine.Skip(2).Select(x => decimal.Parse(x)).Cast<object>().ToList();
+
+                    /*  var data = new List<object>();
+                      data.Add(new DateTime(date.Year, date.Month, date.Day, timeOnly.Hours, timeOnly.Minutes, 0));
+                      data.Add(timeOnly);
+                      data.AddRange(values);*/
+
+                    //  var data = new List<object>(splitedInputLine);
+
+                    var data = new List<object>();
+                    data.Add(splitedInputLine[0]);
+                    data.Add(splitedInputLine[1]);
+                    data.AddRange(values);
+
+                    newData.Add(data);
+
+                }
+
+                if (newData.Count > 0)
+                {
+                    Console.Write("Sending...");
+
+                    ValueRange vr = new ValueRange();
+                    vr.Values = newData;
+
+                    SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(vr, spreadsheetId, range);
+                    request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
+                    request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
+                    var response = request.Execute();
+
+                    Console.WriteLine("DONE");
+                }
+                else
+                {
+                    Console.WriteLine("No new data.");
                 }
             }
-
-            Console.WriteLine("");
-            Console.WriteLine("Sending...");
-
-            ValueRange vr = new ValueRange();
-            vr.Values = newData;
-            //vr.Range = range;
-            //vr.MajorDimension = "ROWS";
-
-            SpreadsheetsResource.ValuesResource.AppendRequest request = service.Spreadsheets.Values.Append(vr, spreadsheetId, range);
-            request.InsertDataOption = SpreadsheetsResource.ValuesResource.AppendRequest.InsertDataOptionEnum.INSERTROWS;
-            request.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.RAW;
-            var response = request.Execute();
-
-
-            Console.WriteLine("DONE");
-
-
         }
 
         private static Sheet CreateSheet(SheetsService service, string spreadsheetId, string sheetName, ref Spreadsheet spreadsheet)
